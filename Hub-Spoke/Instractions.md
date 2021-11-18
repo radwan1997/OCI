5b7fab72744b4aa7a35ba4ff479aebc443cde5c2
radwan1997 <radwanmohamed1997@gmail.com>
2021-11-18 13:29:40 +0200

    update tf files

--- Hub-Spoke/Instractions.md
+++ Hub-Spoke/Instractions.md
@@ -4,3 +4,14 @@
 # (2) Deploy our Code to OCI infra
 
 #              let's begin with prepare our code with Terraform.
+
+our main dir contain these files:
+[1] variables.tf
+[2] provider.tf
+[3] spoke1.tf
+[4] spoke2.tf
+[5] main.tf
+[6] hub.tf
+[7] compute.tf
+[8] datasources.tf
+[9] outputs.tf 
---
+++ Hub-Spoke/datasources.tf
@@ -0,0 +1,27 @@
+# Get list of availability domains
+data "oci_identity_availability_domains" "ADs" {
+  compartment_id = var.tenancy_ocid
+}
+
+# Get the latest Oracle Linux image
+data "oci_core_images" "InstanceImageOCID" {
+  compartment_id           = var.compartment_ocid 
+  operating_system         = var.instance_os
+  operating_system_version = var.linux_os_version
+  shape                    = var.InstanceShape
+
+  filter {
+    name   = "display_name"
+    values = ["^.*Oracle[^G]*$"]
+    regex  = true
+  }
+}
+
+data "oci_identity_region_subscriptions" "home_region_subscriptions" {
+    tenancy_id = var.tenancy_ocid
+
+    filter {
+      name   = "is_home_region"
+      values = [true]
+    }
+}
